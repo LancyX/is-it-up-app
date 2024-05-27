d1fbfa3d0dbd2d4d01c1e48ac1198f11cdf688dc
Anton Shkapa <as@prostopay.net>
2024-05-28 00:13:13 +0300

    add cronjob for testing connection to home server, instead check on api request

--- basic.py
+++ basic.py
@@ -1,5 +1,8 @@
+import os
 import subprocess
 import socket
+from datetime import datetime
+import pytz
 
 def ping(host):
     """
@@ -12,22 +15,46 @@ def ping(host):
     except subprocess.CalledProcessError:
         return False
 
-def telnet(host: str, port: int, timeout=5):
+async def crontask():
+    now = datetime.now(pytz.timezone('Europe/Kyiv'))
+    timestamp = now.strftime("%d/%m %H:%M:%S")
+
+    known_status = read_file("status")
+
+    power = telnet()
+
+    if known_status == power:
+        pass
+    else:
+        write_to_file(source="status", value=power)
+
+    if known_status == power:
+        pass
+    else:
+        write_to_file(source="last-change", value=timestamp)
+
+    write_to_file(source="updated", value=timestamp)
+
+def telnet():
     """
     Check if a specific port on a host is open.
     Returns True if the port is open, False otherwise.
     """
+    host = os.getenv("HOME_HOST")
+    port = int(os.getenv("HOME_PORT"))
+    timeout = 5
+
     try:
         # Create a socket object
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(timeout)
         # Try to connect to the host and port
         sock.connect((host, port))
+        return "1"
     except socket.error:
-        return False
+        return "0"
     finally:
         sock.close()
-    return True
 
 def read_file(source):
     with open(f'static/{source}', 'r', encoding="utf-8") as file:
--- main.py
+++ main.py
@@ -1,40 +1,41 @@
-import os
-from datetime import datetime
-import pytz
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from basic import ping, read_file, write_to_file, telnet
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from contextlib import asynccontextmanager
+from basic import read_file, crontask
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Initialize the scheduler
+scheduler = AsyncIOScheduler()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Set up rate limiting
+
+    # Start the scheduler
+    scheduler.start()
+
+    # Yield control to the application
+    yield
+
+    # Clean up resources on shutdown
+    scheduler.shutdown()
+
+app.router.lifespan_context = lifespan
+
+scheduler.add_job(crontask, 'interval', minutes=1)
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
 
-    now = datetime.now(pytz.timezone('Europe/Kyiv'))
-    timestamp = now.strftime("%d/%m %H:%M:%S")
-
     last_change = read_file("last-change")
-    known_status = read_file("status")
+    power = read_file("status")
+    updated = read_file("updated")
 
-    if telnet(host=os.getenv("HOME_HOST"),
-              port=int(os.getenv("HOME_PORT"))):
-        power = "1"
-    else:
-        power = "0"
-
-    if known_status == power:
-        pass
-    else:
-        write_to_file(source="status", value=power)
-
-    if known_status == power:
-        pass
-    else:
-        write_to_file(source="last-change", value=timestamp)
-        last_change = timestamp
 
     # Determine the image to display based on the power value
     if power == "1":
@@ -59,7 +60,7 @@ async def read_root():
             <h1>вул. Машинобудівників, буд. 23</h1>
             <img src="{image_url}" alt="Power Status" width="150" height="150">
             <p><b>Електропостачання</b>: {power}</p>
-            <p><b>Час оновлення данних</b>: {timestamp}</p>
+            <p><b>Час оновлення данних</b>: {updated}</p>
             <p><b>Остання зміна статусу</b>: {last_change}</p>
         </div>
     </body>
